83c13276efb38c4b77fd7537f1b93a78aa474068
Anca Julean <anca.julean@softvision.ro>
2016-01-21 15:23:23 +0200

    Initial Commit

---
+++ ShapeGame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ShapeGame
+//
+//  Created by Anca Julean on 21/01/16.
+//  Copyright © 2016 Anca Julean. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ShapeGame/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ShapeGame
+//
+//  Created by Anca Julean on 21/01/16.
+//  Copyright © 2016 Anca Julean. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
